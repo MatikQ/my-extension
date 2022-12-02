a39832e59ac7b05a23bd0cdadae1a9d42af21dec
MatikQ <119701721+MatikQ@users.noreply.github.com>
2022-12-02 12:46:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+scene.setBackgroundColor(6)
+effects.confetti.startScreenEffect()
+pause(3000)
+game.setDialogFrame(img`
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    `)
+game.setDialogTextColor(4)
+game.splash("Welcome to good pizza")
+effects.confetti.endScreenEffect()
+music.baDing.playUntilDone()
